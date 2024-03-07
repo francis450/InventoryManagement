93b099b56c998e9f0d4ad4da8009a04e29eaf8a9
Francis Kamande <franciskamande2001@gmail.com>
2024-03-07 10:51:17 +0300

    configuring db connection

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+// Database Configuration
+$config['db']['host'] = 'localhost'; 
+$config['db']['username'] = 'francis';
+$config['db']['password'] = 'root'; 
+$config['db']['database'] = 'inventory_management'; 
+
+// Base URL
+$config['base_url'] = 'http://localhost:8000'; 
+
+// Email Configuration
+// $config['email']['host'] = 'smtp.example.com'; 
+// $config['email']['username'] = 'your_email@example.com'; 
+// $config['email']['password'] = 'your_email_password'; 
+// $config['email']['port'] = 587; 
+
+
+
